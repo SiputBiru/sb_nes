eba08d642353a4aae8aead9aab9996e3d620ecfc
SiputBiru <hillsforrest03@gmail.com>
2026-06-28 21:19:12 +0700

    feat: address mode

--- src/sb_6502/sb_6502.h
+++ src/sb_6502/sb_6502.h
@@ -5,6 +5,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "../sb_bus/sb_bus.h"
+
 // Status register flags (P)
 #define SB_6502_CARRY (1 << 0)
 #define SB_6502_ZERO (1 << 1)
@@ -19,7 +21,7 @@
 typedef struct {
   uint16_t addr;
   bool page_crossed;
-} sb_6502_result;
+} sb_6502_result_t;
 
 typedef struct {
 
@@ -33,10 +35,25 @@ typedef struct {
   bool nmi_pending;
   bool irq_pending;
 
-  sb_6502_result result;
+  sb_6502_result_t result;
 
 } sb_6502_t;
 
-typedef void (*sb_6502_op_fn)(sb_6502_t* cpu, struct sb_bus_t* bus, uint8_t value);
+typedef void (*sb_6502_op_fn)(sb_6502_t* cpu, sb_bus_t* bus, uint8_t value);
+
+sb_6502_result_t addr_immediate(sb_6502_t* cpu, sb_bus_t* bus);        // Implicit (no addr fetch)
+sb_6502_result_t addr_zero_page(sb_6502_t* cpu, sb_bus_t* bus);        // Single byte addr
+sb_6502_result_t addr_zero_page_x(sb_6502_t* cpu, sb_bus_t* bus);      // (zp + X) & 0xFF
+sb_6502_result_t addr_absolute(sb_6502_t* cpu, sb_bus_t* bus);         // Two byte addr
+sb_6502_result_t addr_absolute_x(sb_6502_t* cpu, sb_bus_t* bus);       // (abs + X), check page
+sb_6502_result_t addr_absolute_y(sb_6502_t* cpu, sb_bus_t* bus);       // (abs + Y), check page
+sb_6502_result_t addr_indirect(sb_6502_t* cpu, sb_bus_t* bus);         // JMP only (([addr]))
+sb_6502_result_t addr_indexed_indirect(sb_6502_t* cpu, sb_bus_t* bus); // ((zp + X))
+sb_6502_result_t addr_indirect_indexed(sb_6502_t* cpu, sb_bus_t* bus); // ((zp)) + Y, check page
+sb_6502_result_t
+addr_relative(sb_6502_t* cpu, sb_bus_t* bus);                 // branch instruction (bEQ, BNE, etc.)
+sb_6502_result_t addr_implied(sb_6502_t* cpu, sb_bus_t* bus); // CLC, INX, TAX
+
+sb_6502_result_t addr_accumulator(sb_6502_t* cpu, sb_bus_t* bus); // ASL A, ROL A, etc.
 
 #endif
